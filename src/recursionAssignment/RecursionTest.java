889dd12942e96558670fb3be0497d69ee98bb39b
Xinan Shan <>
2021-05-21 01:01:29 +0000

    red

--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -19,30 +19,33 @@ public class RecursionTest{
     public void Test1(){
         // make assertion statement(s)
         // assertEquals(EXPECTEDVALUE, TESTVALUE);
-      assertEquals(Recursion.array220(new int[]{1, 2, 20}, 0), true);
+      assertEquals(true, Recursion.array220(new int[]{1, 2, 20}, 0));
         // assertTrue(TESTBOOLEAN);
     }
     @Test
     public void Test2(){
-      assertEquals(Recursion.array220(new int[]{3}, 0), false);
+      assertEquals(false, Recursion.array220(new int[]{3}, 0));
     }
     @Test
     public void Test3(){
-      assertEquals(Recursion.array220(new int[]{3, 30}, 0), true);
+      assertEquals(true, Recursion.array220(new int[]{3, 30}, 0));
     }
     @Test
     public void Test4(){
-      assertEquals(Recursion.array220(new int[]{}, 0), false);
+      assertEquals(false, Recursion.array220(new int[]{}, 0));
     }
     @Test
     public void Test5(){
-      assertEquals(Recursion.array220(new int[]{28, 6, 10, 7, 70, 36}, 0), true);
+      assertEquals(true, Recursion.array220(new int[]{28, 6, 10, 7, 70, 36}, 0));
     } 
     @Test
     public void Test6(){
-      assertEquals(Recursion.stringClean("yyzzza"), "yza");
+      assertEquals("yza", Recursion.stringClean("yyzzza"));
     } 
-    
+    @Test
+    public void Test7(){
+      assertEquals("abcd", Recursion.stringClean("abbbcdd"));
+    } 
     // add more tests
     @After
     public void afterTest(){
