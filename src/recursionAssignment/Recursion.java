5069499dd1bea64fe082cb631f25dd26d5f8a28c
Xinan Shan <>
2021-05-21 00:20:03 +0000

    green and refactor

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -4,7 +4,8 @@ public class Recursion{
   public static boolean array220(int[] intNums, int index){
     //declearing the variables
     int Length = intNums.length;
-    
+    if (index >= Length - 1) {
+     return false;
     if (intNums[index] * 10 == intNums[index + 1]) {
      return true;
     }else {
