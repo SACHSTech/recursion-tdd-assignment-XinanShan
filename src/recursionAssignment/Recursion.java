3c13ec4435576c097b7d700c9ed12253fd5fe6da
Xinan Shan <>
2021-05-21 01:35:40 +0000

    change of base case for array220

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -6,11 +6,11 @@ public class Recursion{
     int Length = intNums.length;
     // check if the index is greater than the length of the array
     if (index >= Length - 1) {
+      // base case
      return false;
     }
     // check if the current index position in the array has a value followed in the array by that value times 10
     if (intNums[index] * 10 == intNums[index + 1]) {
-      // base case
      return true;
      // if not, check for the next one and repeat the whole thing
     }else {
@@ -20,7 +20,7 @@ public class Recursion{
   }
   public static String stringClean(String str){
     String answer = "";
-
+    if ( str.ch)
     return answer; 
   }
  // public static boolean strCopies(String str, String sub, int n){
